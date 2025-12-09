cdd5d60c02f797681b8aebcf1d13d5fd594da034
Julio Cundumi <julio.ce.dev@gmail.com>
2025-12-08 20:59:11 -0500

    authpopup in progress

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,16 @@ import type { IState } from './interfaces/IState'
 import PricingPage from './pages/pricing-page/PricingPage'
 import Footer from './components/footer/Footer'
 import TemplatesPopup from './components/templates-popup/TemplatesPopup'
+import AuthPopup from './components/auth-popup/AuthPopup'
+import { useState } from 'react'
 
 function App() {
   const {sidebarOption} = useSelector((state:IState)=>state.sidebar)
   const { templatesPopupOpen } = useSelector(
     (state: IState) => state.toolbarOption
   );
+
+  const [open, setOpen] = useState(true);
 
   return (
     <>
@@ -29,6 +33,7 @@ function App() {
         {/* popups globales */}
         <RemoteInfoBubble/>
         {templatesPopupOpen && <TemplatesPopup/>}
+        {<AuthPopup isOpen={open} onClose={() => setOpen(true)} onAuthSuccess={(user) => { /* redirect */ }} />}
 
         {/* paginas */}
         <Outlet/>
