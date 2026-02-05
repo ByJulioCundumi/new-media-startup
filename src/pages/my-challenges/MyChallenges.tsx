a702d63c73a58f8ef12662026d6cda9a1421c547
Julio Cundumi <julio.ce.dev@gmail.com>
2026-02-05 10:26:24 -0500

    working on activity page

--- src/pages/my-challenges/MyChallenges.tsx
+++ src/pages/my-challenges/MyChallenges.tsx
@@ -1,8 +1,16 @@
+import ChallengeRequested from "../../components/challenge-requested/ChallengeRequested"
+import { mockProposals } from "../../util/challengesMock"
 import "./mychallenges.scss"
 
 function MyChallenges() {
   return (
-    <div>MyChallenges</div>
+    <div className="challenges-page__container">
+              {mockProposals.map((challenge) => (
+                <div className="challenges-page__item">
+                  <ChallengeRequested key={challenge.id} {...challenge} />
+                </div>
+              ))}
+            </div>
   )
 }
 
--- src/pages/supported-challenges/SupportedChallenges.tsx
+++ src/pages/supported-challenges/SupportedChallenges.tsx
@@ -1,8 +1,16 @@
+import ChallengeRequested from "../../components/challenge-requested/ChallengeRequested"
+import { mockProposals } from "../../util/challengesMock"
 import "./supportedchallenges.scss"
 
 function SupportedChallenges() {
   return (
-    <div>SupportedChallenges</div>
+    <div className="challenges-page__container">
+              {mockProposals.map((challenge) => (
+                <div className="challenges-page__item">
+                  <ChallengeRequested key={challenge.id} {...challenge} />
+                </div>
+              ))}
+            </div>
   )
 }
 
